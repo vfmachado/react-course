2c552d5a04a51e77d859dc8518a6f492435ee6df
Vinicius Fritzen Machado <fritzenvinicius@gmail.com>
2020-01-05 13:35:04 -0300

    React with Css Modules

---
+++ styling-css-modules/src/Person/Person.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import styles from './Person.module.css'
+
+
+const person = (props) => {
+
+    return (
+        <div className={styles.Person}>
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old.</p>
+            <p>{props.children}</p>
+            <input type="text" onChange={props.changeName} value={props.name} />
+        </div>
+
+    );
+};
+
+export default person;
